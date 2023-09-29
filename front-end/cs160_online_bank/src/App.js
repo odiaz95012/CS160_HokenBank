644584398fcacdfc14f9e0d1c7c4bb8962df187c
odiaz95012 <odiaz95012@gmail.com>
2023-09-28 21:07:01 -0700

    Adjusted the API call to use axios rather than the vanilla JS fetch call.

--- front-end/cs160_online_bank/src/App.js
+++ front-end/cs160_online_bank/src/App.js
@@ -1,18 +1,24 @@
 import React, {useState, useEffect} from 'react'
-
+import axios, { formToJSON } from 'axios';
 function App() {
   const [customers, setCustomers] = useState([{}]);
 
+  async function deleteCustomer(customer_id){
+    await axios.delete(`http://localhost:8000/deleteCustomer/${customer_id}`).then(
+      console.log("Customer Removed from Database")
+    ).catch(
+      err => console.log(err)
+    )
+  }
+
   useEffect( () => {
-    fetch("http://localhost:8000/getCustomers").then( 
-      res => res.json()
-    ).then(
-      customers => {
-        setCustomers(customers);
-        console.log(customers);
-      })
+    axios.get("http://localhost:8000/getCustomers").then( 
+      response => {
+        setCustomers(response.data)
+      }
+    )
       .catch(
-        err => console.log(err)
+        err => console.log("An error occured: " + err)
       )
   }, [])
   return (
@@ -32,8 +38,9 @@ function App() {
             <li key={i}>Zipcode: {customer.zipcode}</li>
             <li key={i}>Balance Due: {customer.balance_due}</li>
             <li key={i}>Status {customer.customer_id}</li>
+            <li key={i}><button type='submit' onClick={() => deleteCustomer(customer.customer_id)}>Remove Customer</button></li>
           </ul>
-          
+         
         ))   
       )} 
     </div>
