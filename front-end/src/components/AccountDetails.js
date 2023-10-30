9b8f3a80e98a919a707c481639bd86eba3bad232
Huyen Phan <128070162+HuyenPhan-ComputerScience@users.noreply.github.com>
2023-10-30 11:27:23 -0700

    done for showing history payment for account details

--- front-end/src/components/AccountDetails.js
+++ front-end/src/components/AccountDetails.js
@@ -7,9 +7,18 @@ import InternalTransfer from './InternalTransfer';
 import ExternalTransfer from './ExternalTransfer';
 import CloseAccount from './CloseAccount';
 import NavBar from './NavBar';
+import Dropdown from './Dropdown';
 
 function AccountDetails() {
     const { accountID } = useParams();
+    const [selectedNumEntries, setSelectedNumEntries] = useState(0);
+    const [isUserDataLoaded, setIsUserDataLoaded] = useState(false);
+    // this variable controls whether 'Complete', 'Transactions', or 'Payments' are displayed in the corresponding table
+    //Default is 'Complete'
+    const [selectedHistoryOption, setSelectedHistoryOption] = useState('Complete');
+    const [userCompleteHistory, setUserCompleteHistory] = useState([]);
+    const [userTransactionHistory, setUserTransactionHistory] = useState([]);
+    const [userPaymentHistory, setUserPaymentHistory] = useState([]);
 
     const [accountInfo, setAccountInfo] = useState({
         accountID: '',
@@ -17,7 +26,7 @@ function AccountDetails() {
         account_type: ''
     });
 
-    const [isUserDataLoaded, setIsUserDataLoaded] = useState(false);
+
 
     useEffect(() => {
         const fetchAccountDetails = async (accountID) => {
@@ -35,6 +44,128 @@ function AccountDetails() {
         fetchAccountDetails(accountID);
         setIsUserDataLoaded(true);
     }, []);
+
+    //history transaction
+    const getUserCompleteHistory = async (accountID, numberOfEntries, authToken) => {
+        try {
+            const response = await axios.get(`http://localhost:8000/getAccountCompleteHistory/${accountID}/${numberOfEntries}`, {
+                headers: {
+                    'authorization': `Bearer ${authToken}`
+                }
+            });
+            return response.data;
+
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    };
+    const getUserTransactionHistory = async (accountID, numberTransactions, authToken) => {
+        try {
+            const response = await axios.get(`http://localhost:8000/getAccountTransactionHistory/${accountID}/${numberTransactions}`, {
+                headers: {
+                    'authorization': `Bearer ${authToken}`
+                }
+            });
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return null; // Handle the error or return an appropriate value
+        }
+    };
+    const getUserPaymentHistory = async (numberPayments, authToken) => {
+        try {
+            const response = await axios.get(`http://localhost:8000/getAccountPaymentHistory/${accountID}/${numberPayments}`, {
+                headers: {
+                    'authorization': `Bearer ${authToken}`
+                }
+            });
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return null; // Handle the error or return an appropriate value
+        }
+    };
+
+    const [dataToRender, setDataToRender] = useState([]);
+    useEffect(() => {
+        const fetchTransactionsData = async () => {
+            try {
+                const authToken = await getCustomerToken();
+                // Fetch transaction data based on selected option
+                if (selectedHistoryOption === 'Complete') {
+                    const completeHistory = await getUserCompleteHistory(accountID, selectedNumEntries, authToken);
+                    setUserCompleteHistory(completeHistory);
+                    setDataToRender(completeHistory);
+                } else if (selectedHistoryOption === 'Transaction') {
+                    const transactionHistory = await getUserTransactionHistory(accountID, selectedNumEntries, authToken);
+                    setUserPaymentHistory(transactionHistory);
+                    setDataToRender(transactionHistory);
+                } else if (selectedHistoryOption === 'Payment') {
+                    const paymentHistory = await getUserPaymentHistory(accountID, selectedNumEntries, authToken);
+                    setUserPaymentHistory(paymentHistory);
+                    setDataToRender(paymentHistory);
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchTransactionsData();
+    }, [selectedHistoryOption, selectedNumEntries]);
+
+    const renderTableData = (dataToRender, numOfEntries) => {
+        if (!dataToRender || dataToRender.length === 0) {
+            return (
+                <tr>
+                    <td colSpan="5" className="text-center py-4">
+                        <h5>No Payment History</h5>
+                    </td>
+                </tr>
+            )
+        }
+
+        if (numOfEntries === 0) {
+            return dataToRender.map((payment) => (
+                <tr key={payment.transaction_id}>
+                    <th scope='row'>{payment.transaction_id}</th>
+                    <td>{payment.account_id}</td>
+                    <td>{payment.action}</td>
+                    <td>{formatDate(payment.date)}</td>
+                    <td>{payment.amount < 0 ? `-$${Math.abs(payment.amount)}` : `$${payment.amount}`}</td>
+                </tr>
+            ));
+        } else {
+            return dataToRender.slice(0, numOfEntries).map((payment) => (
+                <tr key={payment.transaction_id}>
+                    <th scope='row'>{payment.transaction_id}</th>
+                    <td>{payment.account_id}</td>
+                    <td>{payment.action}</td>
+                    <td>{formatDate(payment.date)}</td>
+                    <td>{payment.amount < 0 ? `-$${Math.abs(payment.amount)}` : `$${payment.amount}`}</td>
+                </tr>
+            ));
+        }
+    };
+    const formatDate = (inputDate) => {
+        const date = new Date(inputDate);
+
+        // Extract the date components
+        const day = date.getDate();
+        const month = date.getMonth() + 1; // Months are 0-based, so add 1
+        const year = date.getFullYear();
+
+        // Pad the day and month with leading zeros if needed
+        const formattedDay = String(day).padStart(2, '0');
+        const formattedMonth = String(month).padStart(2, '0');
+
+        // Create the formatted date string in "MM/DD/YYYY" format
+        const formattedDate = `${formattedMonth}/${formattedDay}/${year}`;
+
+        return formattedDate;
+    }
+
+
 
     const getCustomerToken = async () => {
         const authToken = Cookies.get('authToken');
@@ -58,7 +189,7 @@ function AccountDetails() {
             <NavBar />
 
             {isUserDataLoaded ? (
-                <div className="container my-5" style={{border: '1px solid grey', borderRadius: '15px'}}>
+                <div className="container my-5" style={{ border: '1px solid grey', borderRadius: '15px' }}>
                     {/* <header className="bg-dark py-5">
                         <div className="container px-5">
                             <div className="row gx-5 justify-content-center">
@@ -139,6 +270,57 @@ function AccountDetails() {
                     </div>
                 </header>
             )}
+            <div className='row'>
+                <div className='col-md-6 my-1'>
+                    <div className="d-flex justify-content-start ms-4">
+                        <Dropdown
+                            selectedOption={selectedNumEntries}
+                            onSelectedOption={(option) => setSelectedNumEntries(option)}
+                        />
+                    </div>
+                </div>
+                <div className='col-md-6 my-1'>
+                    <div className="d-flex justify-content-end me-4">
+                        <div className="btn-group" id="historyOptions" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" value="Complete" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" checked={selectedHistoryOption === 'Complete'} onChange={() => setSelectedHistoryOption('Complete')} />
+                            <label className="btn btn-outline-primary" htmlFor="btnradio1">Complete</label>
+
+                            <input type="radio" value="Transaction" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" checked={selectedHistoryOption === 'Transaction'} onChange={() => setSelectedHistoryOption('Transaction')} />
+                            <label className="btn btn-outline-primary" htmlFor="btnradio2">Transactions</label>
+
+                            <input type="radio" value="Payment" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" checked={selectedHistoryOption === 'Payment'} onChange={() => setSelectedHistoryOption('Payment')} />
+                            <label className="btn btn-outline-primary" htmlFor="btnradio3">Payments</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row overflow-auto my-4">
+                <table className="col-md-12 table table-hover mx-4">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope='col'>Transaction ID</th>
+                            <th scope='col'>Account ID</th>
+                            <th scope='col'>Transaction Type</th>
+                            <th scope='col'>Date</th>
+                            <th scope='col'>Amount</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {isUserDataLoaded ? (
+                            renderTableData(dataToRender, selectedNumEntries)
+                        ) : (
+                            <tr>
+                                <td colSpan="5" className="text-center py-4">
+                                    <h5>Fetching data...</h5>
+                                </td>
+                            </tr>
+
+                        )}
+                    </tbody>
+
+                </table>
+            </div>
 
             <footer className="py-5 bg-dark">
                 <div className="container px-5">
